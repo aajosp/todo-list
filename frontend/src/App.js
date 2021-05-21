c86630f2150c8778174496e2a28be3bd1bfaf9c7
robokae <alexanderhom19@gmail.com>
2021-05-20 17:55:54 -0700

    updated SideNavbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.scss';
 
+// imported components
+import Notes from './components/Notes.js';
+import TodoList from './components/TodoList.js';
+import SideNavbar from './components/SideNavbar.js';
+
 function App() {
   return (
     <div className="App">
-      Hello, world!
+      <SideNavbar />
+      {/* <TodoList /> 
+      <Notes /> */}
     </div>
   );
-}
+} 
 
 export default App;
--- frontend/src/components/sideNavbar.js
+++ frontend/src/components/sideNavbar.js
@@ -1,2 +1,78 @@
 // component for side navbar
-import '../styles/sideNavbar.scss'
+import React from 'react';
+import '../styles/SideNavbar.scss';
+
+import { 
+    Event, 
+    Settings,
+    Alarm,
+    AccessAlarm,
+    ErrorOutline
+} from '@material-ui/icons';
+
+class UserInfo extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render(){
+        return(
+            <div className="UserInfo">
+                <Settings />
+                <AccessAlarm />
+                <img className="userProfilePic" src={this.userProfilePicSrc}></img>
+                <button className="settingsButton">
+                    <span class="material-icons-outlined"></span>
+                </button>
+                <h3 className="userName"></h3>
+            </div>
+        );
+        
+    }
+}
+
+class TodoActions extends React.Component{
+    // constructor(props) {
+    //     super(props);
+    // }
+    render(){
+        return (
+        <div className="TodoActions">
+            <Event />
+            <button className="todayButton">Today</button>
+            <Alarm />
+            <button className="allTodoButton">All todos</button>
+            <ErrorOutline />
+            <button className="overdue">Overdue</button>
+        </div>
+        );
+
+    }
+}
+
+// class CustomLists extends React.Component {
+//     //constructor(props){
+//     //    super(props);
+//     //}
+
+//     render() {
+//         return;
+//     }
+// }
+
+//class to create sideNavbar react component
+class SideNavbar extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        return(
+            <div className="SideNavbar">
+                <UserInfo />
+                <TodoActions />
+                {/* <CustomLists />  */}
+            </div>  
+        );
+    }
+}
+
+export default SideNavbar;
